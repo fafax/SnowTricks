5c268f729a93954532785d5ae130b4b9892078d9
fabien hamayon <fabienhamayon@free.fr>
2019-10-09 07:48:01 +0200

    create détail trick

---
+++ src/Controller/DetailTrickController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Trick;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+class DetailTrickController extends AbstractController
+{
+    /**
+     * @Route("/trick/{slug}/{id}", name="detail_trick")
+     * @ParamConverter("trick" ,options={"mapping" :{"slug":"slug","id":"id"}})
+     */
+    public function index(Trick $trick ,UserRepository $userRepo)
+    {
+   
+      $comments = $trick->getComments();
+
+      foreach ($comments as $key => $value) {
+         dump($value);
+      }
+         
+      
+        return $this->render('detail_trick/index.html.twig', [
+            'controller_name' => 'Detail Trick',
+            'trick'=> $trick,
+            "comments" => $comments
+        ]);
+    }
+}
