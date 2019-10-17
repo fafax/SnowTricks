b82bf6d0116df3c5fa93ddec18a91ff45cb092fb
fabien hamayon <fabienhamayon@free.fr>
2019-10-17 11:40:42 +0200

    add function uploadAsset

--- src/services/UploadImgService.php
+++ src/services/UploadImgService.php
@@ -2,6 +2,8 @@
 
 namespace App\services;
 
+use App\Entity\Asset;
+use App\Entity\Trick;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -26,7 +28,7 @@ class UploadImgService
 
         try {
             $pictureFile->move(
-                $this->params->get('picture_directory'),
+                $this->params->get('avatar_directory'),
                 $newFilename
             );
         } catch (FileException $e) {
@@ -34,6 +36,31 @@ class UploadImgService
         }
 
         $user->setAvatarFile($newFilename);
+
+    }
+
+    public function uploadAsset($data, Trick $trick)
+    {
+        $asset = new Asset();
+        $pictureFile = $data;
+        $newFilename = uniqid() . '.' . $pictureFile->guessExtension();
+
+        try {
+            $pictureFile->move(
+                $this->params->get('assets_directory'),
+                $newFilename
+            );
+        } catch (FileException $e) {
+            // ... handle exception if something happens during file upload
+        }
+
+        $asset->setname("test");
+        $asset->setType("image");
+        $asset->setUrl($newFilename);
+        $asset->setTrickId($trick->getId());
+
+        $this->em->persist($asset);
+        $this->em->fluch();
 
     }
 }
