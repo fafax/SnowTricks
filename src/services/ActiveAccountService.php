7ec6d87c1127e14b890cc54c19efc68c935f6981
fabien hamayon <fabienhamayon@free.fr>
2019-10-29 22:23:13 +0100

    add controller active account

--- src/services/ActiveAccountService.php
+++ src/services/ActiveAccountService.php
@@ -3,28 +3,29 @@
 namespace App\services;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ActiveAccountService
 {
 
     private $em;
-    
 
     public function __construct(EntityManagerInterface $em)
     {
         $this->em = $em;
     }
-    
+
     private function generateToken()
     {
-        $unique = uniqueid();
+        $unique = \uniqid();
         $token = md5($unique);
         return $token;
     }
 
-    public function setUserToken(User $user){
+    public function setUserToken(User $user)
+    {
         $user->setToken($this->generateToken());
         $this->em->persist($user);
-        $this->flush();
+        $this->em->flush();
     }
 }
